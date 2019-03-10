98516bd491c8d55e410ceb469170a171049baa78
Preet Dha <manpreetdha+github@gmail.com>
2019-03-09 20:29:50 -0800

    Add search field and apply some css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,25 @@ import MoviesList from './MoviesList';
 import './App.css';
 
 export default class App extends Component {
+  state = {
+    movies: []
+  };
+
   render() {
     return (
       <div className="ui container">
         <h2 className="app-title">Movie Finder</h2>
+        <div className="ui inverted segment">
+          <div className="ui inverted form">
+            <div className="one field">
+              <div className="field">
+                <input placeholder="Enter search term(s)" type="text" />
+              </div>
+            </div>
+            <div className="inline field" />
+            <div className="ui submit button">Submit</div>
+          </div>
+        </div>
         <MoviesList />
       </div>
     );
