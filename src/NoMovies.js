87f7dad85c55814725eb423d6417b1f8f96b38c5
Preet Dha <manpreetdha+github@gmail.com>
2019-03-17 02:13:40 -0700

    Add component to display when search yields no results

--- src/NoMovies.js
+++ src/NoMovies.js
@@ -2,12 +2,16 @@ import React from 'react';
 import { Header, Icon, Segment } from 'semantic-ui-react';
 
 const NoMovies = () => (
-  <Segment size="huge" placeholder>
+  <Segment style={noMoviesStyle} size="huge" placeholder>
     <Header icon>
       <Icon name="search" />
       No movies found with that search. Please try again.
     </Header>
   </Segment>
 );
+
+const noMoviesStyle = {
+  margin: '10% auto 0'
+};
 
 export default NoMovies;
